a42fb0a6a464c5691d8303a74ebdcd7ddc3e682f
fundthmcalculus <polygonguru@gmail.com>
2017-01-10 15:35:22 -0500

    Base code

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    # This is the main body (loop) of the application
+    print("Welcome to chat!")
+    print("Enter IP address to connect:")
+
+
+# Main entry point.
+if __name__ == "__main__":
+    # Call the main function
